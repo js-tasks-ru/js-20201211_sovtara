cd71ca53b994fb08bf19ad175c7f5ac24570a78a
Alexander Tarasov <sovtara@gmail.com>
2020-12-22 12:06:01 +0300

    03-objects-arrays 4-uniq rewrite with Set

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -7,9 +7,19 @@ export function uniq(arr) {
   if (typeof arr === 'undefined') {
     return [];
   }
+  return uniqViaSet(arr);
+}
 
+function uniqViaSet(arr) {
+  let uniq = new Set();
+
+  arr.forEach(value => uniq.add(value));
+
+  return Array.from(uniq.values());
+}
+
+function uniqViaReduce(arr) {
   return arr.reduce((result, element) =>
     result.includes(element) ? result : [...result, element]
   , []);
-
 }
