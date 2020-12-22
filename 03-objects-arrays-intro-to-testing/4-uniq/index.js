4b89a61fd88b62c44c7aac2912336393f188a0a1
Alexander Tarasov <sovtara@gmail.com>
2020-12-22 10:34:28 +0300

    03-objects-arrays-intro-to-testing 4-uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,12 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (typeof arr === 'undefined') {
+    return [];
+  }
+
+  return arr.reduce((result, element) =>
+    result.includes(element) ? result : [...result, element]
+  , []);
 
 }
